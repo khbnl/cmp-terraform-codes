ec38f7a90d65cacca97a9fb038b2b3936b7a32ee
Ranu Jain <65921042+RanuJain131@users.noreply.github.com>
2025-02-06 13:15:41 +0530

    Update main.tf
    Signed-off-by: Ranu Jain <65921042+RanuJain131@users.noreply.github.com>

--- azure-block-blob/main.tf
+++ azure-block-blob/main.tf
@@ -1,11 +1,13 @@
 resource "azurerm_resource_group" "rg" {
+  count = var.create_new_resource_group ? 1 : 0
   name     = var.resource_group_name
   location = var.location
 }
 
 resource "azurerm_storage_account" "storage_account" {
   name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.rg.name
+  ##resource_group_name      = azurerm_resource_group.rg.name
+  resource_group_name = var.create_new_resource_group ? azurerm_resource_group.rg[0].name : data.azurerm_resource_group.rg[0].name
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
