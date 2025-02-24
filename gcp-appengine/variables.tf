9203b43abf0f42ba249ca474f5812d91dc612ea5
Ranu Jain <65921042+RanuJain131@users.noreply.github.com>
2025-02-24 11:41:28 +0530

    Update variables.tf
    Signed-off-by: Ranu Jain <65921042+RanuJain131@users.noreply.github.com>

--- gcp-appengine/variables.tf
+++ gcp-appengine/variables.tf
@@ -3,6 +3,10 @@ variable "project_id" {
   type        = string
 }
 
+variable "project" {
+  description = "The ID of the Google Cloud project."
+  type        = string
+}
 variable "location" {
   description = "The region where App Engine will be created (e.g., us-central1)."
   type        = string
