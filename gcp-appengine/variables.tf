cda99e9bbb3292aa6026c556c75ad6339273426b
Ranu Jain <65921042+RanuJain131@users.noreply.github.com>
2025-02-24 10:49:53 +0530

    Update variables.tf
    Signed-off-by: Ranu Jain <65921042+RanuJain131@users.noreply.github.com>

--- gcp-appengine/variables.tf
+++ gcp-appengine/variables.tf
@@ -8,3 +8,15 @@ variable "location" {
   type        = string
   default     = "asia-east1"
 }
+
+variable "gcp_private_key" { 
+  type = string 
+} 
+
+variable "gcp_cred" { 
+  type = map 
+} 
+
+locals {
+  credential = merge(var.gcp_cred, {private_key = "${var.gcp_private_key}"}) 
+} 
