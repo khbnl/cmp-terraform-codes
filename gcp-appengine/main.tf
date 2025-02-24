44811cad9877be16861a9ed238bf05fa43dd877a
Ranu Jain <65921042+RanuJain131@users.noreply.github.com>
2025-02-24 15:21:39 +0530

    Update main.tf
    Signed-off-by: Ranu Jain <65921042+RanuJain131@users.noreply.github.com>

--- gcp-appengine/main.tf
+++ gcp-appengine/main.tf
@@ -1,10 +1,15 @@
 resource "google_project_service" "app_engine_api" {
-  project = var.project
+  #project = var.project
   service = "appengine.googleapis.com"
   disable_on_destroy = false
 }
 
+resource "google_project" "my_project" {
+  name       = var.project_name
+  project_id = var.project_id
+}
+
 resource "google_app_engine_application" "app" {
-  project     = var.project
+  project     = google_project.my_project.project_id
   location_id = var.location
 }
