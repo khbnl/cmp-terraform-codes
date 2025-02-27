96799abb98e945f107e6cb8a64d0a1f5441afe31
Ranu Jain <65921042+RanuJain131@users.noreply.github.com>
2025-02-27 13:10:50 +0530

    Add files via upload
    Signed-off-by: Ranu Jain <65921042+RanuJain131@users.noreply.github.com>

---
+++ gcp-cloudfunctions/variables.tf
@@ -0,0 +1,43 @@
+variable "gcp_credentials" {
+  description = "The GCP service account credentials."
+  type        = string
+  sensitive   = true
+}
+
+variable "project_id" {
+  description = "The ID of the Google Cloud project."
+  type        = string
+}
+
+variable "location" {
+  type        = string
+  description = "The GCP region where the Cloud Function will be deployed (e.g., us-central1)."
+  default     = "asia-east1"
+}
+
+variable "function_name" {
+  description = "The name of the Cloud Function to be deployed."
+  type        = string
+  default     = "example-name"
+}
+
+variable "runtime" {
+  description = "The runtime environment for the Cloud Function (e.g., nodejs22)."
+  type        = string
+  default     = "nodejs22"
+}
+
+variable "bucket_name" {
+  description = "The name of the existing Cloud Storage bucket containing the function's source code."
+  type        = string
+}
+
+variable "source_filename" {
+  description = "The filename of the function's source code archive within the bucket."
+  type        = string
+}
+
+variable "entry_point" {
+  description = "The entry point of the function code to be executed."
+  type        = string
+}
